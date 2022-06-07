09924a66c96004c8d8ed9f91662c1d65b5f8885d
John <gopal1409@gmail.com>
2022-06-07 12:50:57 +0530

    subnet and public ip

--- vnet.tf
+++ vnet.tf
@@ -1,10 +1,26 @@
 ###create virtual network
 resource "azurerm_virtual_network" "myvnet" {
-  name = "myvnet-1"
-  address_space = [ "10.0.0.0/16" ]
+  name                = "myvnet-1"
+  address_space       = ["10.0.0.0/16"]
   resource_group_name = azurerm_resource_group.rg.name
-  location =  azurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location
 }
-#subnet
+#we need to create a subnet
+resource "azurerm_subnet" "mysubnet" {
+  name                 = "mysubnet-1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 #public ip
+resource "azurerm_public_ip" "mypublicip" {
+  name                = "mypublicip-1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static" ###public ip can be static or dynamic in nature
+
+  tags = {
+    environment = "Production"
+  }
+}
 #nic card
