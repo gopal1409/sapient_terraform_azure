019f200b74ae9c5a8fa86705523aa71c0a3c038c
John <gopal1409@gmail.com>
2022-06-07 16:08:41 +0530

    test

--- linux-virtualmachine.tf
+++ linux-virtualmachine.tf
@@ -3,7 +3,7 @@ resource "azurerm_linux_virtual_machine" "mylinuxvm" {
   name                = "mylinuxvm${count.index}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  computer_name = "devlinux-vm1-${count-index}"
+  computer_name = "devlinux-vm1-${count.index}"
   size                = "Standard_DS1_v2"
   admin_username      = "adminuser"
   network_interface_ids = [
